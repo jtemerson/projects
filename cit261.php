a698248315f2fa7dbf335a6f9ce574bac19c6c3d
jtemerson <eme13001@byui.edu>
2016-05-03 08:54:44 -0600

    This file is a test.

---
+++ cit261.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT']. '/modules/head.php'; ?>
+    </head>
+    <body class="sandbox">
+        <header>
+            <div>
+                <?php include $_SERVER['DOCUMENT_ROOT']. '/modules/header.php'; ?>
+            </div>
+        </header>
+
+        <nav>
+            <div>
+                <?php include $_SERVER['DOCUMENT_ROOT']. '/modules/nav.php'; ?>
+            </div>
+        </nav>
+
+        <main>
+            <div class="container">
+                <h1>Sandbox Code</h1>
+                <p>
+                  Here is where I practice different JavaScript and HTML code. I
+                  Do the right code and then break it so that I completely understand it.
+              </p><br><br>
+
+                <ul>
+                  <li><a href="html_tags.php">HTML Tags: Video, Audio, and Canvas</a></li>
+                </ul>
+            </div>
+        </main>
+
+        <footer>
+            <div>
+                <?php include $_SERVER['DOCUMENT_ROOT']. '/modules/footer.php'; ?>
+            </div>
+        </footer>
+    </body>
+</html>
